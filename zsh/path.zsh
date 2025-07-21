e0dffd76c429857059bdc2d8dc2687ac216b96fd
rspacerr <61034617+rspacerr@users.noreply.github.com>
2025-07-20 22:44:57 -0400

    fix: pnpm path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,3 +1,19 @@
 # !/bin/zsh
 
 export PATH="$PATH:/opt/nvim"
+
+# requires node to be installed
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# requires go
+export PATH=$PATH:/usr/local/go/bin
+
+# pnpm
+export PNPM_HOME="$HOME/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
