034cb2216782597ec71c8710d6ba0711b08725af
rspacerr <61034617+rspacerr@users.noreply.github.com>
2025-10-29 22:44:02 -0400

    fix: cli startup improvement

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -2,10 +2,13 @@
 
 export PATH="$PATH:/opt/nvim"
 
+# https://brookskindle.github.io/reducing-zsh-startup-time-by-400.html
 # requires node to be installed
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+function init_nvm() {
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+}
 
 # requires go
 export PATH=$PATH:/usr/local/go/bin
