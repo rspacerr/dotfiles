eb99fed5f35cdbbb76a5e86bd0846b5502fd45c9
rspacerr <61034617+rspacerr@users.noreply.github.com>
2026-01-04 17:37:49 -0500

    add: tmux + wsl config

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -5,3 +5,4 @@ DOT=${HOME}/.dotfiles
 ln -nsfv ${DOT}/nvim         ${HOME}/.config/nvim
 ln -nsfv ${DOT}/vim/init.vim ${HOME}/.vimrc
 ln -nsfv ${DOT}/zsh/.zshrc   ${HOME}/.zshrc
+ln -nsfv ${DOT}/tmux/.tmux.conf ${HOME}/.tmux.conf
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,12 +4,23 @@ alias ls='ls --color=auto'
 alias la='ls -a'
 alias ll='ls -la'
 
+alias vi='nvim'
 alias vim='nvim'
 
 alias dot='cd ~/.dotfiles/'
+alias dev='cd ~/dev/'
 
-alias ga='git add .'
+alias ga='git add'
 alias gst='git status'
 
 alias python='python3'
 alias py='python3'
+
+# wsl only 
+if uname -r | grep -qi 'Microsoft' ; then
+  # open file explorer
+  alias explorer="explorer.exe ."
+
+  # open vscode
+  alias code="$(wslpath $USERPROFILE)/AppData/Local/Programs/Microsoft\ VS\ Code/bin/code"
+fi
